e91f333a1fb67a06091713b630e8ee939f2d9bca
hung.pv <hphamviet83@gmail.com>
2020-08-28 14:10:51 +0700

    fix

--- index.js
+++ index.js
@@ -2,29 +2,31 @@ const axios = require("axios");
 const fs = require("fs");
 
 const getQuote = async () => {
-  try {
-    const { data } = await axios.get("https://quotes.rest/qod?language=en&quot;);
-    const quote = data.contents.quotes[0].quote;
-    const author = data.contents.quotes[0].author;
+	try {
+		const { data } = await axios.get(
+			"https://quotes.rest/qod?language=en&quot;"
+		);
+		const quote = data.contents.quotes[0].quote;
+		const author = data.contents.quotes[0].author;
 
-    console.log("new quote", `"${quote}"`);
+		console.log("new quote", `"${quote}"`);
 
-    return {
-      quote,
-      author,
-    };
-  } catch (err) {
-    console.error(err.message);
-    return {};
-  }
+		return {
+			quote,
+			author,
+		};
+	} catch (err) {
+		console.error(err.message);
+		return {};
+	}
 };
 
 const generate = async () => {
-  const { quote, author } = await getQuote();
+	const { quote, author } = await getQuote();
 
-  if (!quote) return;
+	if (!quote) return;
 
-  fs.writeFileSync("README.md", `_**${quote}**_\n\n${author}`);
+	fs.writeFileSync("README.md", `_**${quote}**_\n\n${author}`);
 };
 
 generate();
